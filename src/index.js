b7b89cceae3c1dd64c5c9c84e4d171799db84965
nastya <nastyavolkm@tut.by>
2021-03-09 17:06:46 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  } else {
+  return array.reduce(function(a, b){
+      if(a < b){
+         return a;
+              }else{
+          return b;
+      } 
+  }); 
+} 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  } else {
+  return array.reduce(function(a, b){
+      if(a > b){
+         return a;
+              }else{
+          return b;
+      } 
+  }); 
+} 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  } else {
+    return array.reduce(function(a, b){
+  return a+b;
+}) / array.length;
+}
 }
